6993c89d69f51071354a129e80643cd5b6bcc14c
marcuslark <marcus.lark.stahlberg@iths.se>
2020-12-02 09:52:45 +0100

    Marcus Klasser tillagda

---
+++ src/main/java/se/iths/grupp2/animals/Lion.java
@@ -0,0 +1,8 @@
+package se.iths.grupp2.animals;
+import java.util.List;
+
+public class Lion extends Feline {
+    public Lion(String name, List<Food> dislikes, List<Food> likes, int xp, boolean wild, String sound) {
+        super(name, dislikes, likes, xp, wild, sound);
+    }
+}
