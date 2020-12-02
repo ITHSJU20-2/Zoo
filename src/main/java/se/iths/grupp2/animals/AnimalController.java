7270a0c29355a4b4c785c99a613f6fd12e08a64d
Tom Lloyd-Jones <67908875+tljbwdwor@users.noreply.github.com>
2020-12-02 11:05:04 +0100

    Amended feed() in Gorilla, Chimp and Orangutan classes to raise xp by 1 each feed

--- src/main/java/se/iths/grupp2/animals/AnimalController.java
+++ src/main/java/se/iths/grupp2/animals/AnimalController.java
@@ -1,6 +1,8 @@
 package se.iths.grupp2.animals;
 
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
 
 public class AnimalController {
@@ -18,6 +20,10 @@ public class AnimalController {
 
     public void loadAnimals() {
         // Create all the animals in here and add to the list
+        Animal gorilla = new Gorilla("Gorilla", Collections.singletonList(Food.FRUIT),0,0,"Silver");
+        Animal chimpanzee = new Chimpanzee("Chimpanzee",Collections.singletonList(Food.FRUIT),0,0,"Black");
+        Animal orangutan = new Orangutan("Orangutan",Collections.singletonList(Food.FRUIT),0,0,"Orange");
+        animals.addAll(Arrays.asList(gorilla,chimpanzee,orangutan));
     }
 
     public void feed(Animal animal, Food food) {
