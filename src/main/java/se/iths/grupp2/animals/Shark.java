7c11157530f3349c2a9ce48c651cf482c745f856
Kabbej <kabbe.jartun@gmail.com>
2020-12-02 10:52:21 +0100

    updated with methods

--- src/main/java/se/iths/grupp2/animals/Shark.java
+++ src/main/java/se/iths/grupp2/animals/Shark.java
@@ -1,10 +1,25 @@
 package se.iths.grupp2.animals;
 
-public class Shark {
+import java.util.List;
+
+public class Shark extends SeaCreature {
 
     private String chew;
 
-    public Shark(String chew) {
+
+    public Shark(String name, List<Food> likes, int xp, boolean dangerous, String teeth, String fins, String chew) {
+        super(name, likes, xp, dangerous, teeth, fins);
         this.chew = chew;
+    }
+
+    public void pet (){
+        System.out.println("Really, you tried petting a shark? You just lost an arm");
+    }
+
+    public void feed (){
+        setXp((getXp()) + 10);
+        if (getXp() > 100){
+            System.out.println("Thank you for feeding me, soon I'll be big enough to eat you" );
+        }
     }
 }
