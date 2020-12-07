42d0fbca77b52580cd41bbbfdb91446bfaa7148b
Tom Lloyd-Jones <67908875+tljbwdwor@users.noreply.github.com>
2020-12-07 16:01:10 +0100

    Added Test class and Gorilla test template

---
+++ src/test/java/se/iths/grupp2/animals/GorillaTest.java
@@ -0,0 +1,20 @@
+package se.iths.grupp2.animals;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class GorillaTest {
+
+    @Test
+    void getLiked() {
+    }
+
+    @Test
+    void pet() {
+    }
+
+    @Test
+    void feed() {
+    }
+}
