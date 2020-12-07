d39ad510cbfa7c28c8f5915537f230cb9abe0218
Tom Lloyd-Jones <67908875+tljbwdwor@users.noreply.github.com>
2020-12-07 16:03:04 +0100

    Added Orangutan test class

---
+++ src/test/java/se/iths/grupp2/animals/OrangutanTest.java
@@ -0,0 +1,18 @@
+package se.iths.grupp2.animals;
+
+import org.junit.jupiter.api.Test;
+
+class OrangutanTest {
+
+    @Test
+    void getLiked() {
+    }
+
+    @Test
+    void pet() {
+    }
+
+    @Test
+    void feed() {
+    }
+}
