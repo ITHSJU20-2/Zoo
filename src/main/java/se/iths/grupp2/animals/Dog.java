f3da3dd11d299bd7e70a000de6fc9b1ee4ada066
SeanWerner <sean.werner@iths.se>
2020-12-09 14:48:51 +0100

    Added boolean for barking

--- src/main/java/se/iths/grupp2/animals/Dog.java
+++ src/main/java/se/iths/grupp2/animals/Dog.java
@@ -3,7 +3,7 @@ package se.iths.grupp2.animals;
 import java.util.List;
 
 public class Dog extends Canine{
-    public Dog (String name, List<Food> dislikes, List<Food> likes, int xp, boolean barking) {
-            super(name, dislikes, likes, xp, barking);
+    public Dog (String name, List<Food> likes, int xp,boolean barking, boolean howling) {
+        super(name, likes, xp, barking, howling);
     }
 }
--- src/main/java/se/iths/grupp2/animals/Wolf.java
+++ src/main/java/se/iths/grupp2/animals/Wolf.java
@@ -3,7 +3,7 @@ package se.iths.grupp2.animals;
 import java.util.List;
 //Wolf Subclass
 public class Wolf extends Canine{
-    public Wolf(String name, List<Food> dislikes, List<Food> likes, int xp, boolean howling) {
-        super(name, dislikes, likes, xp, howling);
+    public Wolf(String name, List<Food> likes, int xp,boolean barking, boolean howling) {
+        super(name, likes, xp, barking, howling);
     }
 }
