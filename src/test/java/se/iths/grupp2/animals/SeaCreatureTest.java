3919dc0bd69f94016a6ef90369059132d2a424e7
Kabbej <kabbe.jartun@gmail.com>
2020-12-11 10:41:39 +0100

    Added tests

--- src/test/java/se/iths/grupp2/animals/SeaCreatureTest.java
+++ src/test/java/se/iths/grupp2/animals/SeaCreatureTest.java
@@ -1,6 +1,9 @@
 package se.iths.grupp2.animals;
 
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
 
 import java.util.Arrays;
 
@@ -11,8 +14,43 @@ class SeaCreatureTest {
     private final Animal Shark = new Shark ("Shark", Arrays.asList(Food.MEAT), 0, true, "Lots of big teeth", "Delicious fins", "chews a lot");
 
     @BeforeAll
-    public void startOfSeaCreaturesTest (){
+    static void startOfSeaCreaturesTest (){
         System.out.println("Will run before every test");
 
+    }
+
+    @AfterAll
+    static void endOfTest (){
+        System.out.println("All seacreatures has been tested");
+    }
+
+    @AfterEach
+    public void resetXP (){
+        Shark.setXp(0);
+        Goldfish.setXp(0);
+    }
+
+    @Test
+    public void feedShark (){
+        Main.getController().feed(Shark, Food.BERRY);
+        assertEquals(0,Shark.getXp());
+    }
+
+    @Test
+    public void petShark (){
+        Main.getController().pet(Shark);
+        assertNotEquals(2,Shark.getXp() );
+    }
+
+    @Test
+    public void feedGoldFish (){
+        Main.getController().feed(Goldfish, Food.BERRY);
+        assertEquals(2,2);
+    }
+
+    @Test
+    public void petGoldFish (){
+        Main.getController().pet(Goldfish);
+        assertNotEquals(100,2);
     }
 }
